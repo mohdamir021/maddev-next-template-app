b0afd91d0669e17aa96741a9aa37da74cbd70f53
mohdamir021 <amir.ibrahim201200@gmail.com>
2024-02-18 00:18:15 +0800

    feat(app): set app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; // OPTIONAL
+import Head from "next/head";
+import { ChakraProvider } from "@chakra-ui/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <>
+      <Head>
+        <title>Setup All Libraries</title>
+        <meta name="description" content="html5qr test" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
+      <ChakraProvider>{getLayout(<Component {...pageProps} />)}</ChakraProvider>
+    </>
+  );
 }
